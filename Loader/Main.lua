9e0347836bde0a35c399f17b4ad90e819b2665ee
NoLag-ldss <danielhartono99111@gmail.com>
2026-06-15 12:54:53 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/g9wQ4En2/raw"))()
-ID="cd4f4b49-e9c2-4b16-b09f-fbc15b1956e9";
-loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AmhadV99/Speed-Hub-X/refs/heads/main/Speed%20Hub%20X", true))()
